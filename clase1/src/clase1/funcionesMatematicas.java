7186cdfb734a83422a9e6d388cfe8dad116df5a0
javier2344 <69542063+javier2344@users.noreply.github.com>
2020-08-11 19:51:00 -0600

    funciones Matematicas
    se agregaron operaciones Matematicas

---
+++ clase1/src/clase1/funcionesMatematicas.java
@@ -0,0 +1,47 @@
+
+package clase1;
+
+
+public class funcionesMatematicas {
+   
+      
+//        System.out.println("resultado de la operacion es "+Resultado);
+
+    
+        int suma (){
+        
+        int dato1 = 12;
+        int dato2 = 10;
+        int Resultado ;
+        Resultado = dato1 + dato2;
+        return Resultado; 
+
+   }
+        
+        int Resta(){
+        int dato1 = 12;
+        int dato2 = 10;
+        int Resultado ;
+        Resultado = dato1 - dato2;
+        return Resultado; 
+
+            
+       }
+
+        double divicion(){
+
+        int dato1 = 12;
+        int dato2 = 10;
+        int Resultado ;
+        Resultado = dato1 /dato2;
+        
+        return Resultado;
+        
+        
+}  }
+        
+      
+
+    
+        
+        
